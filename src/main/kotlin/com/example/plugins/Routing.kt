2fe9389b744a8f72f5017f6d8aaa20c0cd2191b0
Alex Smochina <alexandru.smochina.7e7@itb.cat>
2024-05-03 13:19:04 +0200

    COMMIT

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -3,9 +3,11 @@ package com.example.plugins
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import routes.*
 
 fun Application.configureRouting() {
     routing {
+        customerRouting()
         get("/") {
             call.respondText("Hello World!")
         }
